f4a49482c0595c27b304adab2239e74e3e726bea
BIBANG BEFENE Joseph Donovan <donovan.bibang@aninf.ga>
2023-02-16 15:45:40 +0100

    updates

--- src/app/admin/views/liste-user/liste-user.component.ts
+++ src/app/admin/views/liste-user/liste-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CoreService } from 'src/app/core/services/core.service';
 import { ModalCreateUserComponent } from '../../components/modal-create-user/modal-create-user.component';
 
 @Component({
@@ -9,13 +10,19 @@ import { ModalCreateUserComponent } from '../../components/modal-create-user/mod
 })
 export class ListeUserComponent implements OnInit {
 
-	constructor(private modal: NgbModal) { }
+	constructor(private modal: NgbModal, private coreService: CoreService) { }
 
 	ngOnInit(): void {
-
+		this.getUsers()
 	}
 
 	addUser() {
 		const modal = this.modal.open(ModalCreateUserComponent, { size: 'lg', backdrop: 'static' })
+	}
+
+	getUsers() {
+		this.coreService.get('users').then((res: any) => {
+			console.log(res)
+		})
 	}
 }
