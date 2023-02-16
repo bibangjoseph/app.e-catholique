8de5afbca559f21118ab7ff12caa56f9ff8d1c34
BIBANG BEFENE Joseph Donovan <donovan.bibang@aninf.ga>
2023-02-16 12:59:47 +0100

    crud role

--- src/app/admin/views/liste-role/liste-role.component.ts
+++ src/app/admin/views/liste-role/liste-role.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CoreService } from 'src/app/core/services/core.service';
+import Swal from 'sweetalert2';
 import { ModalCreateRoleComponent } from '../../components/modal-create-role/modal-create-role.component';
 import { ModalUpdateRoleComponent } from '../../components/modal-update-role/modal-update-role.component';
 
@@ -46,6 +47,36 @@ export class ListeRoleComponent implements OnInit {
 		modal.result.catch((reason) => {
 			this.isLoad = true
 			this.getRoles()
+		})
+	}
+
+	deleteRole(item: any) {
+		Swal.fire({
+			icon: 'question',
+			title: 'Suppression',
+			html: `
+			<i class="bi bi-trash icon-md"></i> <br>
+			<h5> "${item.denomination}" </h5>
+			<p> Souhaiez-vous vraiment supprimer ce rôle ?</p>
+			`,
+			showDenyButton: true,
+			showCancelButton: false,
+			confirmButtonText: 'Oui',
+			denyButtonText: `Non`,
+		}).then((result: any) => {
+			if (result.isConfirmed) {
+				this.coreService.delete('role/' + item.id).then((res: any) => {
+					if (res.success === true) {
+						this.coreService.successToast(res.data)
+						this.isLoad = true;
+						this.getRoles()
+					} else {
+						this.coreService.errorToast(res.data)
+						this.isLoad = true;
+						this.getRoles()
+					}
+				})
+			}
 		})
 	}
 }
